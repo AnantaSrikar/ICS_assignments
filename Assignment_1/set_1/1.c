d3e51bf1f6d8bb172051cb90deff3107dde8cd3f
AnantaSrikar <srikarananta01@gmail.com>
2020-01-31 10:29:15 +0530

    Updated for submission style

--- Assignment_1/set_1/1.c
+++ Assignment_1/set_1/1.c
@@ -2,13 +2,12 @@
 int main()
 {
     int n, sum = 0;
-    printf("Enter n : ");
     scanf("%d", &n);
     for (int i =1; i <= n; i++)
     {
         sum += i;
     }
 
-    printf("Sum of %d nos = %d\n", n, sum);
+    printf("%d", sum);
     return(0);
 }
