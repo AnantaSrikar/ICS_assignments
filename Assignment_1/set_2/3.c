c0ba622c3a9f3c5a93f635047eaf2af8e895a92f
ES-19-507 <srikarananta01@gmail.com>
2020-01-31 15:00:32 +0530

    Updated for submission

--- Assignment_1/set_2/3.c
+++ Assignment_1/set_2/3.c
@@ -3,17 +3,13 @@ int main()
 {
     int a, b;
     
-    printf("Enter the value of a : ");
-    scanf("%d", &a);
-
-    printf("Enter the value of b : ");
-    scanf("%d", &b);
+    scanf("%d, %d", &a, &b);
 
     a = a + b;
     b = a - b;
     a = a - b;
 
-    printf("Swap successful....\na = %d\nb = %d\n", a, b);
+    printf("%d, %d", a, b);
     
     return(0);
 }
