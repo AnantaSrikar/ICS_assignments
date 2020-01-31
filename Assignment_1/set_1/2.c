59dc930677c85505a415e813bfafcfb79f984ea7
AnantaSrikar <srikarananta01@gmail.com>
2020-01-31 10:33:24 +0530

    Updated for submission format

--- Assignment_1/set_1/2.c
+++ Assignment_1/set_1/2.c
@@ -2,22 +2,22 @@
 int main()
 {
     char ch;
-    printf("Enter a character : ");
+
     scanf("%c", &ch);
     
     if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
     {
-        printf("ALPHABET\n");
+        printf("ALPHABET");
     }
 
     else if (ch >= '0' && ch <= '9')
     {
-        printf("NUMBER\n");
+        printf("NUMBER");
     }
 
     else 
     {
-        printf("SYMBOL\n");
+        printf("SYMBOL");
     }
 
     return(0);
