501c1460040a45a8f3152b5ce43dc6c57f4d1a8c
ES-19-507 <srikarananta01@gmail.com>
2020-01-31 15:04:31 +0530

    Updated for submission

--- Assignment_1/set_2/5.c
+++ Assignment_1/set_2/5.c
@@ -3,14 +3,13 @@ int main()
 {
     int n, bin = 0;
 
-    printf("Input a number : ");
     scanf("%d", &n);
 
     for (int i = n; i != 0; i /= 2)
     {
         bin++;
     }
-    printf("bits = %d\n", bin);
+    printf("%d", bin);
 
     return(0);
 }
