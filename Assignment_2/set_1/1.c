e5a44dd7682111a44be967454e91bfc77a07da02
AnantaSrikar <srikarananta01@gmail.com>
2020-02-07 09:22:50 +0530

    Added CI func

--- Assignment_2/set_1/1.c
+++ Assignment_2/set_1/1.c
@@ -5,11 +5,14 @@ int main()
     const int t = 10;
     const float r = 3.5;
     float p;
+
     float SI(float, float, int);
+    float CI(float, float, float, int);
 
     scanf("%f", &p);
     
-    printf("Simple Interest for 10 years: %f", SI(p, r, t));
+    printf("Simple Interest for 10 years: %f\n", SI(p, r, t));
+    printf("Compound Interest for 10 years annually: %f\n", CI(p, r, t, 1));
 
     return (0);
 }
@@ -18,4 +21,10 @@ float SI(float p, float r, int t)
 {
     float I = (p*r*t)/100;
     return I;
+}
+
+float CI(float p, float r, float t, int n)
+{
+    float CI = p * (pow((1 + r/(100*n)), (n * t)) - 1 );
+    return CI;
 }
