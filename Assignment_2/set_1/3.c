c785aebcef0aca9fafce333be90e057798c1b621
AnantaSrikar <srikarananta01@gmail.com>
2020-02-10 10:36:27 +0530

    made small changes

--- Assignment_2/set_1/3.c
+++ Assignment_2/set_1/3.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n, sum = 0;
-    scanf("%d", &n);
-    if(n < 0)
-        printf("Entered number is out of range");
-    
-    else
+    int num, n, sum = 0;
+    scanf("%d", &num);
+
+    if (num >= 0 && num < 1000)
     {
-        while(n != 0)
+        n = num;
+        while(n > 0)
         {
             sum += n % 10;
             n /= 10;
         }
-        printf("Sum of the digits of 546 is %d", sum);
+        printf("Sum of the digits of %d is %d", num, sum);
+    }
+    
+    else
+    {
+        printf("Entered number is out of range");
     }
 
-    return (0);
+    return(0);
 }
