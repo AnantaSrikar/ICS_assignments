db889d081c1607d148869b13a3e8c2d03af9795e
AnantaSrikar <srikarananta01@gmail.com>
2020-02-10 11:32:28 +0530

    Started 1st sol

---
+++ Assignment_2/set_3/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a, b, c;
+
+    scanf("%f,%f,%f", &a, &b, &c);
+
+    if(((b*b) - 4*a*c)) >=0)
+        {
+            
+        }
+
+    return(0);
+}
