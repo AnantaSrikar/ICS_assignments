11e986aa48889f4ae70de84ee5bda55ad1a87536
AnantaSrikar <srikarananta01@gmail.com>
2020-02-10 10:53:52 +0530

    Added base sol, pending

--- Assignment_2/set_2/3.c
+++ Assignment_2/set_2/3.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int num, n, sum = 0;
-    scanf("%d", &num);
+    int d, m, y, rem = 0;
+    scanf("%d, %d, %d", &d, &m, &y);
 
-    if (num >= 0 && num < 1000)
-    {
-        n = num;
-        while(n > 0)
-        {
-            sum += n % 10;
-            n /= 10;
-        }
-        printf("Sum of the digits of %d is %d", num, sum);
-    }
+    if(y % 4 != 0)
+        printf("Not a leap year");
+        //find no of days left in year + 365*n(find it using rem) + 31 + 29
     
     else
     {
-        printf("Entered number is out of range");
+        if(m < 2)
+            rem = 60 - d;
+        
+        else if(m == 2)
+            rem = 29 - d;
+        
+        else
+        {
+            //find no of days left in year + 365*2 + 31 + 29
+        }
     }
-
+    
     return(0);
 }
