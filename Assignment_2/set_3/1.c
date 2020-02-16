2b2d04cad48921ff100394381cc6f3ec162e6e94
AnantaSrikar <srikarananta01@gmail.com>
2020-02-16 19:03:13 +0530

    Added 1st solution

--- Assignment_2/set_3/1.c
+++ Assignment_2/set_3/1.c
@@ -6,10 +6,23 @@ int main()
 
     scanf("%f,%f,%f", &a, &b, &c);
 
-    if(((b*b) - 4*a*c)) >=0)
-        {
-            
-        }
+    if((((b*b) - 4*a*c)) >= 0)
+    {
+        float x1, x2;
+        x1 = (-b + sqrt((b*b) - 4 * a * c))/(2 * a) ;
+        x2 = (-b - sqrt((b*b) - 4 * a * c))/(2 * a) ;
+
+        printf("%0.1f, %0.1f", x1, x2);
+    }
+
+    else
+    {
+        float r, i;
+        r = -b / (2 * a);
+        i = sqrt(4 * a * c - (b*b))/(2 * a);
+        printf("%0.1f+i%0.1f, %0.1f-i%0.1f", r, i, r, i);
+    }
+    
 
     return(0);
 }
