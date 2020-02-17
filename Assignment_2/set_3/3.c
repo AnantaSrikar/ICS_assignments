9d980672298b2a20d174e71cd14f53f982e6faf4
AnantaSrikar <srikarananta01@gmail.com>
2020-02-17 18:02:21 +0530

    Partly working

--- Assignment_2/set_3/3.c
+++ Assignment_2/set_3/3.c
@@ -6,9 +6,43 @@ int main()
 
     scanf("%d %d %d, %d %d %d", &d1, &m1, &y1, &d2, &m2, &y2);
 
-    if(y1 <= y2)
+    if(y1 > y2)
     {
-        y_diff = y2 - y1;
+        if(m2 > m1)
+        {
+            y_diff = y1 - y2 - 1;
+
+            if(d2 > d1)
+            {
+                m_diff = 11 - m2 + m1;
+                d_diff = 29 - d1 + d2;
+            }
+
+            else if (d2 <= d1)
+            {
+                m_diff = 12 - m2 + m1;
+                d_diff = d1 - d2 - 1;
+            }
+
+            printf("%d" ,(d_diff + m_diff*30 + y_diff*360));
+        }
+
+        else
+        {
+            y_diff = y1 - y2;
+            m_diff = m1 - m2;
+        }
+        
+    }
+
+    else if (y1 == y2)
+    {
+
+    }
+
+    else
+    {
+        
     }
     
     return(0);
