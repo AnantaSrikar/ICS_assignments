b213cb7495d85156ad6890092c51c3c5fb87347e
AnantaSrikar <srikarananta01@gmail.com>
2020-02-20 01:31:16 +0530

    Added 3rd solution

--- Assignment_2/set_2/3.c
+++ Assignment_2/set_2/3.c
@@ -2,25 +2,45 @@
 int main()
 {
     int d, m, y, rem = 0;
+    int monthDays[] = [31 ,28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
     scanf("%d, %d, %d", &d, &m, &y);
 
-    if(y % 4 != 0)
-        printf("Not a leap year");
-        //find no of days left in year + 365*n(find it using rem) + 31 + 29
-    
-    else
+    if(m > 12 || m < 1 || d > monthDays[m - 1] || d < 1)
+        printf("INVALID");
+
+    if(y % 4 == 0)
     {
-        if(m < 2)
-            rem = 60 - d;
-        
+        if(m == 1)
+            rem = 29 + 31 - d;
+
         else if(m == 2)
             rem = 29 - d;
-        
+
         else
         {
-            //find no of days left in year + 365*2 + 31 + 29
+            rem = (monthDays[m - 1] - d);
+
+            for (int i = m ; i < 12; i++)
+                rem += monthDays[i];
+            
+            rem += 3 * 365 + 31 + 29;
         }
     }
+
+    else
+    {
+        int rem_years = 3 - (y % 4);
+
+        rem = monthDays[m - 1] - d;
+
+        for (int i = m ; i < 12; i++)
+            rem += monthDays[i];
+        
+        rem += rem_years * 365 + 31 + 29;
+    }
+
+    printf("%d", rem);
     
     return(0);
 }
