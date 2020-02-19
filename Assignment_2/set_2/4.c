d8c1cd1b50878a588fd695f479fecd9c63e7eac2
AnantaSrikar <srikarananta01@gmail.com>
2020-02-20 01:40:57 +0530

    Pending 4th solution

---
+++ Assignment_2/set_2/4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float taxRate;
+    long int salary, rent, savings;
+
+    scanf("%ld, %ld, %ld", &salary, &rent, &savings);
+
+    printf("\n%ld, %ld, %ld", salary, rent, savings);
+
+    return (0);
+}
