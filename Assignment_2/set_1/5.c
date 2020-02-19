e64b31b1adfe38d92f0943cf5ccbc4baddd08011
AnantaSrikar <srikarananta01@gmail.com>
2020-02-20 02:02:24 +0530

    Close to solution

--- Assignment_2/set_1/5.c
+++ Assignment_2/set_1/5.c
@@ -2,20 +2,40 @@
 #include<math.h>
 int main()
 {
-    float n;
-    void bin_rep(float);
+    float num, n;
+    int binRep[4];
 
     scanf("%f", &n);
 
-    bin_rep(n);
+    num = n;
+    for(int i = 0; i < 4; i++)
+    {
+        printf("Loop : %d\n", i);
+        if(n * 2 > 1)
+        {
+            binRep[i] = 1;
+            n = (n * 2) - 1;
+        }
+
+        else if(n * 2 == 1)
+        {
+            binRep[i] = 1;
+            break;
+        }
+
+        else
+        {
+            binRep[i] = 0;
+            n = (n * 2) - 1;
+        }
+
+        printf("n = %f\nbinRep[%d] = %d\n", n, i, binRep[i]);
+    }
+
+    printf("Binary equivalent of %f is 0.", num);
+
+    for (int i = 3; i <= 0; i--)
+        printf("%d", binRep[i]);
 
     return(0);
-}
-
-void bin_rep(float n)
-{
-    if(n != 1)
-        bin_rep((n * 2) - floor(n*2));
-    
-    printf("%f", floor(n*2));
 }
