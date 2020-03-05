173982f791135cf71f20f6092ac71bceded756ac
AnantaSrikar <srikarananta01@gmail.com>
2020-03-05 18:26:55 +0530

    Updated logic

--- Assignment_4/set_3/5.c
+++ Assignment_4/set_3/5.c
@@ -1,17 +1,31 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int a, b,cf = 0;
+    int a, b,cf = 0, cnt = 0;
     scanf("%d, %d", &a, &b);
+
+    int len_a = log10(a);
+    int len_b = log10(b);
     
-    while(a > 0 && b > 0)
+    while(len_a > 0 || len_b > 0)
     {
-        if((a % 10) + (b % 10) > 9)
-            cf++;
+        if((a % 10) + (b % 10) + cf > 9)
+        {
+            cnt++;
+            cf = ((a % 10) +(b % 10) / 10);
+        }
+        else
+            cf = 0;
+        
         a /= 10;
         b /= 10;
-    }
-
-    printf("%d", cf);
+        
+        if(len_b > 0)
+            len_b--;
+        if(len_a > 0)
+            len_a--;
+        }
+    printf("%d", cnt);
     return (0);
 }
